b41f193d0b1b8daa18bc027510e24fc707f78e04
swift-man <jiniopening@gmail.com>
2022-10-09 14:34:45 +0900

    Add files via upload

---
+++ UIKit-Code-Based-Project/UIKit-Code-Based-Project/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  UIKit-Code-Based-Project
+//
+//  Created by SwiftMan on 2022/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    
+    view.backgroundColor = .systemRed
+  }
+
+
+}
+
